2b627dce5318b334ac40887e0048bfa667932df6
Njagalavibe <patricolumu@gmail.com>
2023-06-30 17:36:42 +0300

    changes to Js file

--- game.js
+++ game.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
+
 const prompt = require("prompt-sync")({ sigint: true });
 let choices = {
-=======
-const prompt=require("prompt-sync")({sigint:true});
-// checkout https://www.geeksforgeeks.org/javascript-is-showing-reference-error-prompt-is-not-defined/
-// javascript is designed to run in the browser (client), so in the browser the prompt
-// funciton exists on the window object
-// to run the game server side( as a script/not in the browser), we use node js (serverside version on js)
-// make sure you have node installed
-// in terminal run, 
-// node game.js
-
-let choices =  {
->>>>>>> 7dc30449a32a0e80e9d66fa201078517a1db564e
     paper: "paper",
     scissors: "scissors",
     rock: "rock"
