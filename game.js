449126e1baff985205401dcfed8f5321f21ae32e
Njagalavibe <patricolumu@gmail.com>
2023-06-30 17:24:16 +0300

    Changes to js file

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-
-let choices =  {
+const prompt = require("prompt-sync")({ sigint: true });
+let choices = {
     paper: "paper",
     scissors: "scissors",
     rock: "rock"
@@ -9,50 +9,47 @@ let choices =  {
 function getComputerChoice(choices) {
     let computerChoice = Object.keys(choices);
     let randomComputerChoice = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-    return `Computer choice : ${randomComputerChoice}`;
+    return randomComputerChoice;
 }
-console.log(getComputerChoice(choices));
+getComputerChoice(choices);
 
 
 /*Get player choice*/
 function userChoice() {
-    
-    if (userInput === "paper" ||userInput === "scissors"||userInput=== "rock" ) {
-        return `Player choice : ${userInput}`;
-}else{
-    return "Please use valid matrix"
-}
+
+    if (userInput === "paper" || userInput === "scissors" || userInput === "rock") {
+        return userInput;
+
+    } else {
+        return "Please use valid matrix"
+    }
 }
 let userInput = prompt("please enter you chocie: ")
-console.log(userChoice());
+userChoice();
+
+/*return choices*/
+function showChoices() {
+    let computerChoices = getComputerChoice(choices)
+    let userChoiceS = userChoice()
+    console.log(`computer choice : ${computerChoices}\n\
+user choice : ${userChoiceS}`)
+}
+showChoices()
 
 /*Play a round*/
-function  playRound(userGuess, computerGuess){
-    if (userGuess === computerGuess ){
-        return "It's a Tie"; 
-    } 
-    if (userGuess === "rock"){
-        if (computerGuess === "paper"){
-            return "You Lose";
-        }else{
-            return "You Win";
-        }
-    }
-    if (userGuess === "paper"){
-        if (computerGuess === "scissors"){
-            return "You Lose";
-        }else{
-            return "You Win";
-        }
-    }
-    if (userGuess === "scissors"){
-        if (computerGuess === "rock"){
-            return "You Lose";
-        }else{
-            return "You Win";
-        }
+function playRound(userGuess, computerGuess) {
+    if (userGuess === computerGuess) {
+        return " it's a Tie";
+    } else if ((userGuess === "rock" && computerGuess === "paper") ||
+        (userGuess === "scissors" && computerGuess === "rock") ||
+        (userGuess === "paper" && computerGuess === "scissors")) {
+        return "You Lose";
+    } else if ((userGuess === "paper" && computerGuess === "rock") ||
+        (userGuess === "rock" && computerGuess === "scissors") ||
+        (userGuess === "scissors" && computerGuess === "paper")) {
+        return "You Win";
     }
 }
-let userGuess =  userChoice();
-let computerGuess =  getComputerChoice(choices);
+let userGuess = userChoice();
+let computerGuess = getComputerChoice(choices);
 console.log(playRound(userGuess, computerGuess));          
