849afbe14492e4095ce6ef95229ae7d989c07d97
Njagalavibe <patricolumu@gmail.com>
2023-07-14 18:24:29 +0300

    refactor the ui and code

--- game.js
+++ game.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 /*const prompt = require("prompt-sync")({ sigint: true });*/
+=======
+const prompt = require("prompt-sync")({ sigint: true });
+>>>>>>> master
 let choices = ["paper","scissors","rock"]
 /*Get computer choice*/
 function getComputerChoice(choices) {
@@ -6,13 +10,15 @@ function getComputerChoice(choices) {
     let randomComputerChoice = choices[Math.floor(Math.random() * choices.length)];
     return randomComputerChoice;
 }
-getComputerChoice(choices);
+(getComputerChoice(choices));
 
 
 /*Get player choice*/
 function userChoice() {
 
-    if (userInput === "paper" || userInput === "scissors" || userInput === "rock") {
+    if( (userInput == "paper" )||
+        (userInput == "scissors" )|| 
+        (userInput == "rock")) {
         return userInput;
 
     } else {
@@ -26,28 +32,48 @@ userChoice();
 function showChoices() {
     let computerChoices = getComputerChoice(choices)
     let userChoiceS = userChoice()
-    console.log(`computer choice : ${computerChoices}\n\
-user choice : ${userChoiceS}`)
+    console.log(`computer choice : ${computerChoices}`)
 }
 showChoices()
 
 /*Play a round*/
 function playRound(userGuess, computerGuess) {
-    if (userGuess === computerGuess) {
+    if (computerGuess === userGuess) {
         return " it's a Tie";
-    } else if ((userGuess === "rock" && computerGuess === "paper") ||
-        (userGuess === "scissors" && computerGuess === "rock") ||
-        (userGuess === "paper" && computerGuess === "scissors")) {
+    } else if ((computerGuess == "paper" && userGuess == "rock") ||
+        (computerGuess == "rock" &&  userGuess == "scissors") ||
+        (computerGuess == "scissors" && userGuess == "paper")) {
         return "You Lose";
-    } else if ((userGuess === "paper" && computerGuess === "rock") ||
-        (userGuess === "rock" && computerGuess === "scissors") ||
-        (userGuess === "scissors" && computerGuess === "paper")) {
-        return "You Win";
+    } else  if (( userGuess == "paper" && computerGuess == "rock" ) ||
+    (userGuess == "paper" && computerGuess == "rock" ) ||
+    (userGuess == "scissors" &&  computerGuess == "paper")) {
+    return "You Win";
     }
 }
 let userGuess = userChoice();
 let computerGuess = getComputerChoice(choices);
+<<<<<<< HEAD
 console.log(playRound(userGuess, computerGuess));  
 
 
 export {choices,getComputerChoice,userChoice,showChoices,playRound};
+=======
+console.log(playRound(userGuess, computerGuess)); 
+
+
+//Will add playing more rounds(5 rounds each stake) after i work on the ui as i consider that a more excitng chaalenge
+/*
+function fiveRounds(){
+    computerScore=0;
+    playerScore=0;
+    for (let round=0; round<=5;round++){
+        playRound(userGuess, computerGuess) 
+    }
+    
+}
+console.log(fiveRounds())
+*/
+
+
+
+>>>>>>> master
